defbb288a82b7476b80ba9d328199bb7323dbaef
esther parnes <eparnes123@gmail.com>
2020-12-06 17:56:47 +0200

    Add project files.

---
+++ MaalotOOPSession11/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaalotOOPSession11
+{
+   class Student
+   {
+      public Student(string name)
+      {
+         Name = name;
+      }
+
+      public string Name { get; set; }
+
+
+   }
+}
